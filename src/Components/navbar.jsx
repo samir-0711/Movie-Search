27acb6f9f6dbbd998f7e5106b83ce05a9867935c
Samir Prajapati <prajapatisamir200@gmail.com>
2020-12-27 19:45:58 +0530

    Typo Corrected

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="#">
-          Vidly
+          Movie Search
         </Link>
         <button
           className="navbar-toggler"
